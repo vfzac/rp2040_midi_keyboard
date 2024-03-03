fca4c67795e62bfa1ed75c1a7b8118f3494357eb
vfz <thefritzvalzado@gmail.com>
2024-03-03 22:21:11 +0800

    added octave var to make changing octave easier in the future

--- code.py
+++ code.py
@@ -16,10 +16,11 @@ encoder_handler = EncoderHandler()
 keyboard.modules = [encoder_handler]
 keyboard.modules.append(MidiKeys())
 
-encoder_handler.pins = (
-    # regular direction encoder and a button
-    (board.GP14, board.GP15, board.GP13,), # encoder #1 
-    )
+# ************removing encoder for now*******************
+# encoder_handler.pins = (
+#     # regular direction encoder and a button
+#     (board.GP14, board.GP15, board.GP13,), # encoder #1 
+#     )
 
 keyboard.row_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5,)
 keyboard.col_pins = (board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15,)
@@ -34,7 +35,7 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 #	3	C	D	E	F	G	A	B	
 #									
 
-#	layout2	G3 to B5	melodica	
+#	layout2	F3 to B5	melodica	
 #		9	10	11	12	13	14	15	
 #									
 #	0				F#	G#	A#		
@@ -45,6 +46,7 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 #	5	C5	D	E	F	G	A	B	
 #									
 # *might or might not add
+
 #  |6|7|
 # -|-|-|
 # 8|a|b|
@@ -56,20 +58,23 @@ def testprint(key, keyboard, *args):
     print("ee")
 
 velo = 80
+octave = 60 # C4/Middle C - add or minus to get note
 # KC.NO, = disabled key 
 keyboard.keymap = [
-    [KC.MIDI_NOTE(61, velo), KC.MIDI_NOTE(63, velo), KC.NO,					 KC.MIDI_NOTE(66, velo), KC.MIDI_NOTE(68, velo), KC.MIDI_NOTE(70, velo), KC.NO,
-     KC.MIDI_NOTE(60, velo), KC.MIDI_NOTE(62, velo), KC.MIDI_NOTE(64, velo), KC.MIDI_NOTE(65, velo), KC.MIDI_NOTE(67, velo), KC.MIDI_NOTE(69, velo), KC.MIDI_NOTE(71, velo),
-     KC.MIDI_NOTE(73, velo), KC.MIDI_NOTE(75, velo), KC.NO,					 KC.MIDI_NOTE(78, velo), KC.MIDI_NOTE(80, velo), KC.MIDI_NOTE(82, velo), KC.NO,
-     KC.MIDI_NOTE(72, velo), KC.MIDI_NOTE(74, velo), KC.MIDI_NOTE(76, velo), KC.MIDI_NOTE(77, velo), KC.MIDI_NOTE(79, velo), KC.MIDI_NOTE(81, velo), KC.MIDI_NOTE(82, velo),
+    [KC.NO,                  KC.NO,                  KC.NO,					 KC.MIDI_NOTE(octave - 6, velo), KC.MIDI_NOTE(octave - 4, velo), KC.MIDI_NOTE(octave - 2, velo), KC.NO,
+     KC.NO,                  KC.NO,                  KC.NO,                  KC.MIDI_NOTE(octave - 7, velo), KC.MIDI_NOTE(octave - 5, velo), KC.MIDI_NOTE(octave - 3, velo), KC.MIDI_NOTE(octave - 1, velo),
+     KC.MIDI_NOTE(octave + 1, velo), KC.MIDI_NOTE(octave + 3, velo), KC.NO,					 KC.MIDI_NOTE(octave + 6, velo), KC.MIDI_NOTE(octave + 8, velo), KC.MIDI_NOTE(octave + 10, velo), KC.NO,
+     KC.MIDI_NOTE(octave, velo), KC.MIDI_NOTE(octave + 2, velo), KC.MIDI_NOTE(octave + 4, velo), KC.MIDI_NOTE(octave + 5, velo), KC.MIDI_NOTE(octave + 7, velo), KC.MIDI_NOTE(octave + 9, velo), KC.MIDI_NOTE(octave + 11, velo),
+     KC.MIDI_NOTE(octave + 13, velo), KC.MIDI_NOTE(octave + 15, velo), KC.NO,					 KC.MIDI_NOTE(octave + 18, velo), KC.MIDI_NOTE(octave + 20, velo), KC.MIDI_NOTE(octave + 22, velo), KC.NO,
+     KC.MIDI_NOTE(octave + 12, velo), KC.MIDI_NOTE(octave + 14, velo), KC.MIDI_NOTE(octave + 16, velo), KC.MIDI_NOTE(octave + 17, velo), KC.MIDI_NOTE(octave + 19, velo), KC.MIDI_NOTE(octave + 21, velo), KC.MIDI_NOTE(octave + 22, velo),
      ]
 ]
 
-BENDDOWN = KC.NO
-BENDUP = KC.NO
-encoder_handler.map = [ (( BENDDOWN, BENDUP, KC.MIDI_NOTE(60, velo)),)] # Layer 1
-BENDDOWN.after_press_handler(testprint)
-BENDUP.after_press_handler(testprint)
+# BENDDOWN = KC.NO
+# BENDUP = KC.NO
+# encoder_handler.map = [ (( BENDDOWN, BENDUP, KC.MIDI_NOTE(60, velo)),)] # Layer 1
+# BENDDOWN.after_press_handler(testprint)
+# BENDUP.after_press_handler(testprint)
 
 if __name__ == '__main__':
     keyboard.go()
